283468bd070ecf14599bf5d3bdc58267beeefff6
Mohib <mohibullahmohim2020@gmail.com>
2024-09-05 03:44:20 +0600

    brand product added

--- src/App.jsx
+++ src/App.jsx
@@ -75,27 +75,28 @@ function App() {
 
         {categoryArray.map((category, index) => (
           <TabPanel key={index}>
-            <h2>{category} content</h2>
-           {  filteredData.length > 0 ? (
+            {filteredData.length > 0 ? (
               <div className="grid grid-cols-3 gap-4">
                 {filteredData.map((item) => (
                   <div key={item._id} className="p-4 max-w-md">
                     <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                       <img
                         className="h-48 w-full object-cover object-center"
-                        src={item.imageUrl || "https://dummyimage.com/720x400"}
-                        alt={item.title}
+                        src={item.photoUrl || "https://dummyimage.com/720x400"}
+                        alt={item.name}
                       />
                       <div className="p-6">
                         <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
                           CATEGORY {category}
                         </h2>
                         <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                          {item.title}
+                          {item.name}
                         </h1>
-                        <p className="leading-relaxed mb-3">{item.description}</p>
+                        <p className="leading-relaxed mb-3">
+                          {item.description}
+                        </p>
                         <div className="flex items-center flex-wrap">
-                          <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                          <a href="" className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
                             Learn More
                             <svg
                               className="w-4 h-4 ml-2"
@@ -111,33 +112,10 @@ function App() {
                             </svg>
                           </a>
                           <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                            <svg
-                              className="w-4 h-4 mr-1"
-                              stroke="currentColor"
-                              strokeWidth={2}
-                              fill="none"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              viewBox="0 0 24 24"
-                            >
-                              <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-                              <circle cx={12} cy={12} r={3} />
-                            </svg>
-                            1.2K
+                         price : {item.price}
                           </span>
                           <span className="text-gray-400 inline-flex items-center leading-none text-sm">
-                            <svg
-                              className="w-4 h-4 mr-1"
-                              stroke="currentColor"
-                              strokeWidth={2}
-                              fill="none"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              viewBox="0 0 24 24"
-                            >
-                              <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
-                            </svg>
-                            6
+                            {item.brand}
                           </span>
                         </div>
                       </div>
